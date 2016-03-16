b22c4d5d39d02c7af738151bd3e97872401b78da
VitaHub <hubenko_vitalii@mail.ua>
2016-03-16 00:25:36 +0000

    Finish log in/log out

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,17 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  
   get 'help' => 'static_pages#help'
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
-  get 'signup' => 'users#new'
   
+  get 'signup' => 'users#new'
   resources :users
+  
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
+  
   
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
